8ec5f275c646329181b857f8a9ea5a4acb0de415
Rodiyz <malinkin54ru@gmail.com>
2022-01-26 21:29:18 +0700

    ~

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
+let newYears = document.getElementById('footer-copy__date');
+function footerDate() {
+  let today = new Date();
+  const newYearsDate = today.getFullYear();
+  newYears.innerHTML = newYearsDate;
+}
 
+footerDate();
